1f91ac1a53bf3146ead0c48b60f0806eea9e038c
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2020-09-18 17:43:31 -0700

    Updated structure to remove Okta.

--- src/index.js
+++ src/index.js
@@ -6,19 +6,11 @@ import {
   useHistory,
   Switch,
 } from 'react-router-dom';
-import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
-
-import 'antd/dist/antd.less';
 
 import { NotFoundPage } from './components/pages/NotFound';
 import { ExampleListPage } from './components/pages/ExampleList';
-import { ProfileListPage } from './components/pages/ProfileList';
 import { LoginPage } from './components/pages/Login';
-import { HomePage } from './components/pages/Home';
 import { LandingPage } from './components/pages/Landing';
-import { ExampleDataViz } from './components/pages/ExampleDataViz';
-import { config } from './utils/oktaConfig';
-import { LoadingComponent } from './components/common';
 
 ReactDOM.render(
   <Router>
@@ -41,22 +33,13 @@ function App() {
   };
 
   return (
-    <Security {...config} onAuthRequired={authHandler}>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/implicit/callback" component={LoginCallback} />
-        <Route path="/landing" component={LandingPage} />
-        {/* any of the routes you need secured should be registered as SecureRoutes */}
-        <SecureRoute
-          path="/"
-          exact
-          component={() => <HomePage LoadingComponent={LoadingComponent} />}
-        />
-        <SecureRoute path="/example-list" component={ExampleListPage} />
-        <SecureRoute path="/profile-list" component={ProfileListPage} />
-        <SecureRoute path="/datavis" component={ExampleDataViz} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Security>
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/landing" component={LandingPage} />
+      {/* any of the routes you need secured should be registered as SecureRoutes */}
+      <Route path="/" exact component={() => <LandingPage />} />
+      <Route path="/example-list" component={ExampleListPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 }
