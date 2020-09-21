531b467ecd0f7398cf4f194a415fb2cc186abc36
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-21 14:26:51 -0500

    Created files for the sign up form

---
+++ src/components/pages/SignUp/SignUpContainer.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import RenderSignUp from './RenderSignUp';
+
+const SignUpContainer = () => {
+  useEffect(() => {});
+
+  return <RenderSignUp />;
+};
+
+export default SignUpContainer;
