231e305cb028eecd845e0ab2bc882fd72a82eaf4
sofia Luna <Sofia21luna@gmail.com>
2020-09-21 14:27:53 -0600

    ugly starting form

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 5,
+    backgroundColor: 'powderblue',
+  },
+  inputs: {
+    padding: '10px',
+  },
+}));
 
 const LoginContainer = () => {
-  return <Container maxWidth="sm"></Container>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm" className={classes.root}>
+      <h2>Log In</h2>
+      <form>
+        <Grid xs={12} spacing={3}>
+          <TextField
+            className={classes.inputs}
+            id="outlined-basic"
+            label="Name"
+            variant="filled"
+          />
+        </Grid>
+        <Grid xs={12} spacing={3}>
+          <TextField
+            className={classes.inputs}
+            id="filled-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="filled"
+          />
+        </Grid>
+      </form>
+    </Container>
+  );
 };
 
 export default LoginContainer;
