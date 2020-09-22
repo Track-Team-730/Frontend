c81c0611655978abf9a1051508bcedca47f3c20a
Ashton <ashtoturn@gmail.com>
2020-09-22 12:57:01 -0500

    most of the edit form functionality complete

--- src/forUsing/components/userItemsLIsted.js
+++ src/forUsing/components/userItemsLIsted.js
@@ -1,21 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axiosWithAuth from '../../state/utils/axiosWithAuth';
 
-const UserItemsListed = () => {
-  const editItem = () => {};
+const initialEditItem = {
+  name: '',
+  description: '',
+  price: '',
+  market: '',
+  product: '',
+};
 
-  const saveEdited = () => {};
+const UserItemsListed = () => {
+  const [editing, setEditing] = useState(false);
+  const [itemToEdit, setItemToEdit] = useState(initialEditItem);
+
+  const editItem = () => {
+    setEditing(true);
+    setItemToEdit();
+  };
+
+  const saveEdited = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put('/item/itemId', itemToEdit)
+      .then(res => {})
+      .catch(err => console.log(err));
+  };
 
   const deleteItem = () => {
     axiosWithAuth()
-      .delete('')
+      .delete('/item/itemId')
       .then(res => console.log(res))
       .catch(err => console.log(err));
   };
   return (
     <>
       <h1>User Items here!</h1>
+
+      <form onSubmit={saveEdited}>
+        <label>
+          Name:&nbsp;
+          <input
+            onChange={{}}
+            value={e => setItemToEdit({ ...itemToEdit, name: e.target.value })}
+          />
+        </label>
+        <label>
+          Description:&nbsp;
+          <input
+            onChange={{}}
+            value={e =>
+              setItemToEdit({ ...itemToEdit, description: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          Price:&nbsp;
+          <input
+            onChange={{}}
+            value={e => setItemToEdit({ ...itemToEdit, price: e.target.value })}
+          />
+        </label>
+        <label>
+          Market:&nbsp;
+          <select
+            onChange={{}}
+            value={e =>
+              setItemToEdit({ ...itemToEdit, market: e.target.value })
+            }
+          >
+            <option value="">- select an option -</option>
+          </select>
+        </label>
+        <label>
+          Product:&nbsp;
+          <select
+            onChange={{}}
+            value={e =>
+              setItemToEdit({ ...itemToEdit, product: e.target.value })
+            }
+          >
+            <option value="">- select an option -</option>
+          </select>
+        </label>
+      </form>
+
       <button>Edit Item</button>
+      <button>Save Item</button>
       <button>Delete Item</button>
       <button>Cancel</button>
     </>
