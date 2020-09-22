9a7c8d292dee477af10276c667fc18bcb4b8d867
Ashton <ashtoturn@gmail.com>
2020-09-22 17:09:18 -0500

    fleshed out user item list

--- src/forUsing/components/userPage.js
+++ src/forUsing/components/userPage.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosWithAuth from '../../state/utils/axiosWithAuth';
 import AddItem from './addItem';
 import PriceComparison from './priceComparison';
 import UserItemsListed from './userItemsLIsted';
 
 const UserPage = () => {
+  const [userItemsList, setUserItemsList] = useState([]);
+
+  const getUserItems = () => {
+    axiosWithAuth()
+      .get('/user/items')
+      .then(res => {
+        setUserItemsList(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getUserItems();
+  }, []);
   return (
     <>
       <h1>Welcome HUMAN SO AND SO</h1>
       <h2>User Page</h2>
       <AddItem />
       <PriceComparison />
-      <UserItemsListed />
+      <UserItemsListed
+        updateItems={setUserItemsList}
+        userItemsList={userItemsList}
+      />
     </>
   );
 };
