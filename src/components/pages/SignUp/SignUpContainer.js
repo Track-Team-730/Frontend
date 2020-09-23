ce10b93c6406d148512eb5e9448eca12ff36584f
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-23 18:01:26 -0500

    Deleted the leftover commented out code for the time component from the sign up container

--- src/components/pages/SignUp/SignUpContainer.js
+++ src/components/pages/SignUp/SignUpContainer.js
@@ -25,9 +25,6 @@ const initialValues = {
 const initialUsers = [];
 const initialDisabled = true;
 
-//Time
-//const API_KEY = 'JZN77L5UEHBN';
-
 const SignUpContainer = () => {
   //states
   const [users, setUsers] = useState(initialUsers);
@@ -36,9 +33,6 @@ const SignUpContainer = () => {
   );
   const [disabled, setDisabled] = useState(initialDisabled);
   // const [formErrors, setFormErrors] = useState(initialFormErrors)
-  //Time states
-  //const [unixTimeStamp, setUnixTimeStamp] = useState();
-
 
   //Helpers
   let history = useHistory();
@@ -82,40 +76,6 @@ const SignUpContainer = () => {
 
   //console.log(formErrors.name);
 
-  //Time
-  // useEffect(() => {
-  //   axios
-  //       .get(`http://api.timezonedb.com/v2.1/list-time-zone?key=${API_KEY}&format=json`)
-  //       .then(res => {
-  //           //console.log(res);
-  //           //setTimeData(res.data); 
-  //           //console.log(timeData);
-  //           // console.log(res.data);
-  //           // console.log(res.data.zones);
-  //           // console.log(res.data.zones[203]);
-  //           // console.log(res.data.zones[203].timestamp);
-  //           setUnixTimeStamp(res.data.zones[203].timestamp);
-  //           // console.log(unixTimeStamp);
-  //       })
-  //       .catch(err => {
-  //           console.log("ERROR:", err);
-  //       });
-  // }, [unixTimeStamp]);
-
-//SK 1600931035 [422]
-//ZA 1600905835
-//KE [203]
-
-  // //unixTime = Math.floor(Date.now() / 1000);
-  // const convertTime = unixTimeStamp => {
-  //     let milliseconds = (unixTimeStamp * 1000) + (3600000 * 5); //3600000
-  //     let dateObject = new Date(milliseconds);
-  //     let timeCountry = dateObject.toLocaleString();
-  //     return (timeCountry);
-  // };
-
-  // let timeCountry = convertTime(unixTimeStamp);
-
   return (
     <div>
       <Container maxWidth="sm">
@@ -128,10 +88,6 @@ const SignUpContainer = () => {
         />
         <Time />
       </Container>
-      {/* <div>
-        <h4>Date and Time in Kenya</h4>
-        <p>{timeCountry}</p>
-      </div> */}
     </div>
   );
 };
